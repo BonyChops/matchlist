6e132b8b705eda44dbdf2b83c62fe664c37f3373
Bony_Chops / Sota Suzuki <office.kentarou@gmail.com>
2022-08-30 13:40:25 +0000

    Edit: spotify flow

--- functions/src/ownApis.js
+++ functions/src/ownApis.js
@@ -4,12 +4,21 @@ const { db } = require('../firestore');
 
 // matchlist APIs
 const ownApis = (fastify, _, done) => {
-    fastify.post('/matchlist', async (req, reply) => {
+    fastify.get('/matchlist', async (req, reply) => {
         if (req.session.spotifyToken === undefined) {
             reply.status = 400;
             reply.send({ error: 'UNAUTHORIZED', mes: 'Unauthorized session.' });
             return;
         }
+        const result = await axios.get('https://api.spotify.com/v1/me', {
+            headers: {
+                authorization: `Bearer ${req.session.spotifyToken}`
+            },
+            validateStatus: () => true
+        });
+
+        const doc = await db.collection('matchlists').add({test: "test"});
+        await doc.collection('playlists').add({test:" test"});
 
     });
 
